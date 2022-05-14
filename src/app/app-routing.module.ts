a2210c6a9de951e54e1a882fb0414566ea921634
noelhorvath <hnoel2000@gmail.com>
2022-05-14 13:34:41 +0200

    remove generated template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
     {
         path: '',
-        loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
     }
 ];
 
