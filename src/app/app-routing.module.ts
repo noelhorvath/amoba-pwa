2cb5f26cd1722eeb1ff2a720f7ac4ad9011c9414
noelhorvath <hnoel2000@gmail.com>
2022-05-15 16:39:18 +0200

    add service worker and add new paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomeModule } from './pages/home/home.module';
 
 const routes: Routes = [
     {
+        path: 'home',
+        loadChildren: (): Promise<HomeModule> => import('./pages/home/home.module').then(m => m.HomeModule)
+    },
+    {
         path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'home',
+        pathMatch: 'full'
     }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,26 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameEngineService } from './services/game-engine/game-engine.service';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
     declarations: [AppComponent],
     entryComponents: [],
-    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-    providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ServiceWorkerModule.register(
+        'ngsw-worker.js',
+        {
+            enabled: environment.production,
+            // Register the ServiceWorker as soon as the application is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: 'registerWhenStable:30000'
+        })
+    ],
+    providers: [
+        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+        GameEngineService
+    ],
     bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule { }
