ac4ce129864827921a3ece54218079d2cfddc7df
noelhorvath <hnoel2000@gmail.com>
2022-05-25 00:33:55 +0200

    change tab bar button's icon and ripple effect color

--- src/app/pages/home/tab-bar/tab-bar.component.ts
+++ src/app/pages/home/tab-bar/tab-bar.component.ts
@@ -6,13 +6,24 @@ import { Component, EventEmitter, Output } from '@angular/core';
     styleUrls: ['./tab-bar.component.scss'],
 })
 export class TabBarComponent {
+    public colors: { [key: string]: string };
     @Output() public tabSelect: EventEmitter<string>;
 
     public constructor() {
+        this.colors = { game: 'primary', settings: 'primary' };
         this.tabSelect = new EventEmitter<string>();
     }
 
     public tabChangeHandler(event: { tab: string }): void {
+        Object.keys(this.colors).map( (key: string) => this.colors[key] = 'primary' );
+        switch (event.tab) {
+            case 'game':
+                this.colors[event.tab] = 'secondary';
+                break;
+            case 'settings':
+                this.colors[event.tab] = 'secondary';
+                break;
+        }
         this.tabSelect.emit(event.tab);
     }
 }
